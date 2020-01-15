8ccdb636db6fd587dd02bda8d89255c10903c7cc
CAILIANGYONG <1007438209@qq.com>
2020-01-15 22:43:58 +0800

    add index and bootstrap

--- src/main/java/com/example/community/controller/HelloController.java
+++ src/main/java/com/example/community/controller/HelloController.java
@@ -3,14 +3,15 @@ package com.example.community.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestAttribute;
 import org.springframework.web.bind.annotation.RequestParam;
-
+/*
+ by 2020/1/15
+ */
 @Controller
 public class HelloController {
-    @GetMapping("/hello")
-    public String hello(@RequestParam(name="name") String name, Model model)
-    {   model.addAttribute("name",name);
-        return "hello";
+    @GetMapping("/")
+    public String index()
+    {
+        return "index";
     }
 }
